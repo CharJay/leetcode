9c9f82718b60f8bfddc6fc4aa4dd6cccd2ba9e57
charjay <820419196@qq.com>
2019-01-27 17:29:57 +0800

    addTwoNumbers

--- src/main/java/com.charjay/question2/AddTwoNumbers.java
+++ src/main/java/com.charjay/question2/AddTwoNumbers.java
@@ -17,45 +17,79 @@ public class AddTwoNumbers {
      * @return
      */
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        //
-        Map map = new HashMap();
         ListNode tempNode1=l1;
         ListNode tempNode2=l2;
-        int i=0;
-        int sum=0;
         int tempValue=0;
-        int jinWei=0;
-        do{
-            sum+=tempValue+(tempNode1.val+tempNode2.val)%10+jinWei;//求余数
-            tempValue=(tempNode1.val+tempNode2.val)/10;//求进位数
+        int jin=0;
+        int yu=0;
 
-            jinWei=jinWei*10;
-            map.put(i,tempNode1.val+tempNode2.val);
-            i++;
+        ListNode sumNode = null;
+        ListNode retNode = null;
+        do{
+            if(tempNode1 == null){
+                tempNode1 = new ListNode(0);
+            }
+            if(tempNode2 == null){
+                tempNode2 = new ListNode(0);
+            }
+            yu = (tempNode1.val + tempNode2.val + jin) % 10;//求余数
+            tempValue = yu;// + jin + jinTemp
+            jin=(tempNode1.val+tempNode2.val + jin)/10;//求进位数
+
+            //重新计算
+
+            if(sumNode == null){
+                sumNode = new ListNode(tempValue);
+                retNode = sumNode;
+            }else {
+                while (sumNode.next !=null){
+                    sumNode = sumNode.next;
+                }
+                sumNode.next = new ListNode(tempValue);
+            }
             tempNode1=tempNode1.next;
             tempNode2=tempNode2.next;
-        }while (tempNode1!=null);
+        }while (tempNode1!=null || tempNode2!=null);
 
+        if(jin>0){
+            while (sumNode.next !=null){
+                sumNode = sumNode.next;
+            }
+            sumNode.next = new ListNode(jin);
+        }
 
-        System.out.println(sum);
-        System.out.println(map);
-        return null;
+        return retNode;
     }
 
     public static void main(String[] args) {
-        ListNode node1 = new ListNode(2);
-        ListNode node2 = new ListNode(4);
+//        ListNode node1 = new ListNode(2);
+//        ListNode node2 = new ListNode(4);
+//        node2.next=node1;
+//        ListNode node3 = new ListNode(3);
+//        node3.next=node2;
+//
+//        ListNode node4 = new ListNode(5);
+//        ListNode node5 = new ListNode(6);
+//        node5.next=node4;
+//        ListNode node6 = new ListNode(4);
+//        node6.next=node5;
+
+
+        ListNode node1 = new ListNode(9);
+        ListNode node2 = new ListNode(9);
         node2.next=node1;
-        ListNode node3 = new ListNode(3);
+        ListNode node3 = new ListNode(8);
         node3.next=node2;
 
-        ListNode node4 = new ListNode(5);
-        ListNode node5 = new ListNode(6);
-        node5.next=node4;
-        ListNode node6 = new ListNode(4);
-        node6.next=node5;
+        ListNode node4 = new ListNode(2);
 
-        ListNode ret = new AddTwoNumbers().addTwoNumbers(node3, node6);
-        System.out.println(ret);
+
+        ListNode ret = new AddTwoNumbers().addTwoNumbers(node3, node4);
+        System.out.print(ret.val+"->");
+        while (ret.next !=null){
+            System.out.print(ret.next.val+"->");
+            ret = ret.next;
+        }
+//        System.out.println(ret.val+"->"+ret.next.val+"->"+ret.next.next.val);
     }
 }
